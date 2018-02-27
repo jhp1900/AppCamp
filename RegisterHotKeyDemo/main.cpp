aa99cab2607dee939352e3dcf12db85300dfeefa
Xjhp-INC <jhp1900@hotmail.com>
2018-02-27 10:29:00 +0800

    + 添加注册全局快捷键的测试项目

---
+++ RegisterHotKeyDemo/main.cpp
@@ -0,0 +1,27 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <Windows.h>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    HWND hConsole = GetActiveWindow();
+
+    MSG msg = {0};
+    RegisterHotKey(hConsole,1,MOD_CONTROL | MOD_SHIFT,'D');
+
+    while (GetMessage(&msg, NULL, 0, 0) != 0){
+        if (msg.message == WM_HOTKEY){
+            if(msg.wParam == 1)
+                OutputDebugString(L"jfkdsjkfdas\n");
+        }
+    }
+
+    return app.exec();
+}
